7c8e41e4d1d5244ed8a6dc6cb62f008d6c5fba10
82244859 <build-jh.kim@kt.com>
2024-07-02 14:28:29 +0900

    feat: 최초 커밋 및 spring security default 설정

---
+++ src/main/java/com/inp/msa/inpmsaauth/InpMsaAuthApplication.java
@@ -0,0 +1,13 @@
+package com.inp.msa.inpmsaauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InpMsaAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InpMsaAuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/inp/msa/inpmsaauth/InpMsaAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inp.msa.inpmsaauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InpMsaAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
